4f20152e1aece0a2ecacf746f4f015c9b85f740e
ThomasWesterlaken <82945745+ThomasWesterlaken@users.noreply.github.com>
2022-10-24 14:49:50 +0200

    added auto-inc

--- Kwik Kwek Snack/KwikKwekSnack.Data/Migrations/20221024124705_addingautoincrement2.Designer.cs
+++ Kwik Kwek Snack/KwikKwekSnack.Data/Migrations/20221024124705_addingautoincrement2.Designer.cs
@@ -4,6 +4,7 @@ using KwikKwekSnack.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KwikKwekSnack.Data.Migrations
 {
     [DbContext(typeof(SnackContext))]
-    partial class SnackContextModelSnapshot : ModelSnapshot
+    [Migration("20221024124705_addingautoincrement2")]
+    partial class addingautoincrement2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Kwik Kwek Snack/KwikKwekSnack.Data/Order.cs
+++ Kwik Kwek Snack/KwikKwekSnack.Data/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace KwikKwekSnack.Data
 {
     public class Order
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OrderID { get; set; }
         public Status Status { get; set; }
         public bool TakeAway { get; set; }
